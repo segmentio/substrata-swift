6c85285da0dd4b697a14fb112d0e1af79c172a14
Brandon Sneed <brandon@redf.net>
2024-05-23 11:56:45 -0700

    Bump tvOS version to minimum of 12.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .macOS("10.15"),
         .iOS("13.0"),
-        .tvOS("11.0")
+        .tvOS("12.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
